be375e8e8a13bc8671e18fbff5bea1a20ffc866f
Ndech7 <stephennderitu3@gmail.com>
2023-08-27 10:02:59 +0300

    Fetch ISS External API

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ let map = L.map("map").setView([0, 0], 1);
 let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy: OpenStreetMap",
 }).addTo(map);
+
+// fetch an external API - https://api.wheretheiss.at/v1/satellites/25544
+const url = "https://api.wheretheiss.at/v1/satellites/25544";
+const marker = L.marker();
+getISSPos();
+
+async function getISSPos() {
+  const response = await fetch(url);
+  const data = await response.json();
+  const { latitude, longitude } = data;
+  marker.setLatLng([latitude, longitude]).addTo(map);
+}
